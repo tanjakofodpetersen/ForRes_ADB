7e64e13db12c056022f8b41c790452b4df1bf058
Tanja Petersen <tanja.petersen@artsdatabanken.no>
2023-02-06 14:26:02 +0100

    Bedre farger

--- Aronia_HH.R
+++ Aronia_HH.R
@@ -15,8 +15,6 @@ aronia %>%
   rename('Foer revisjon' = 'Foer_revisjon' ,
          'Etter revisjon' = 'Etter_revisjon' ) %>%
   make_long(`Foer revisjon`, `Etter revisjon`) %>%
-  #mutate(across(c(node, next_node),
-  #              ~ordered(.x, levels = c("Ikke risikovurdert tidligere","NR","NK","LO","PH","HI","SE")))) %>%
                   ggplot(., aes(x = x, 
                                 next_x = next_x, 
                                 node = node, 
@@ -25,12 +23,12 @@ aronia %>%
                                 label = node)) +
                     geom_sankey(flow.alpha = 0.75, node.color = 0.9) +
                     geom_sankey_label(size = 2, color = 1, fill = "white") +
-                    scale_fill_manual(values = c("Aronia arbutifolia"="#d7191c",
-                                                 "Aronia melanocarpa"="#abd9e9",  # Noe er feil her?
-                                                 "Aronia xprunifolia"="#fdae61",
+                    scale_fill_manual(values = c("Aronia arbutifolia"="#d7191c",  #lightcoral
+                                                 "Aronia melanocarpa"="#abd9e9",  #steelblue3
+                                                 "Aronia xprunifolia"="#fdae61",   #khaki1
                                                  "Aronia sp."="gray60",
                                                  "Aronia eller Sorbaronia"="gray60",
-                                                 "Sorbaronia mitschurinii"="#2c7bb6"),
+                                                 "Sorbaronia mitschurinii"="#2c7bb6"),  #olivedrab4
                                       name = "") +
                     labs(x = "") +
                     theme_sankey(base_size = 10) +
